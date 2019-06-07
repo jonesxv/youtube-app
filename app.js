191e3bf0e6055add7b8750ce8bbc90e5047557e2
Ryan Jones <ryanjones33@gmail.com>
2019-06-07 09:38:26 -0700

    Added partials

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var path = require('path');
  
 var app = express();
  
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({
+  partialsDir  : [
+    //  path to your partials
+    path.join(__dirname, 'views/partials'),
+  ]
+}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
