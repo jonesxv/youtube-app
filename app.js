a212df80665f131f930fcf1e4e9eb513653a7152
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-06-07 15:05:57 -0700

    add new styles for the header

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const firebase = require('firebase');
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var path = require('path');
 const test = require('./public/js/test')
 const keys = require('./config');
@@ -10,11 +10,11 @@ const authAPI = require('./public/js/auth')
 let currentUser = '';
 
 var firebaseConfig = {
-  apiKey: keys.dbKey,
-  authDomain: "app-galvanize.firebaseapp.com",
-  databaseURL: "https://app-galvanize.firebaseio.com",
-  projectId: "youtube-app-galvanize",
-  appId: "1:142923113115:web:911d986153c88176"
+    apiKey: keys.dbKey,
+    authDomain: "app-galvanize.firebaseapp.com",
+    databaseURL: "https://app-galvanize.firebaseio.com",
+    projectId: "youtube-app-galvanize",
+    appId: "1:142923113115:web:911d986153c88176"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -24,37 +24,35 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 
 function addVideo() {
-  db.collection('playlist')
-    .doc('videos')
-    .set({
-      href: 'test.com',
-      title: 'Title Test'
-    });
+    db.collection('playlist')
+        .doc('videos')
+        .set({
+            href: 'test.com',
+            title: 'Title Test'
+        });
 }
 
 var app = express();
 
 app.engine('handlebars', exphbs({
-  partialsDir  : [
-    //  path to your partials
-    path.join(__dirname, 'views/partials'),
-  ]
+    partialsDir: [
+        //  path to your partials
+        path.join(__dirname, 'views/partials'),
+    ]
 }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  // authAPI.youtubeAPI().then(sections => {
-  //   console.log(sections)
+app.get('/', function(req, res) {
+    // authAPI.youtubeAPI().then(sections => {
+    //   console.log(sections)
 
-  
-    
-  // });
-  res.render('home',{sections: [1,2,3,4]});
-  
+
+
+    // });
+    res.render('home', { sections: [1, 2, 3, 4] });
+
 });
-
-addVideo();
 
 app.listen(3000);
