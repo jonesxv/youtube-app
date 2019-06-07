0911aa3a368f65cfdeb9632c8eff24519633e9be
Ryan Jones <ryanjones33@gmail.com>
2019-06-07 12:08:35 -0700

    database config

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,13 +1,18 @@
 const keys = require('../../config');
 const fetch = require('node-fetch');
+const defaults = require('./defaults');
 
-function youtubeAPI(query) {
-  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=${keys.apiKey}`)
-  .then(res => {
-    return res.json()
-  }).then(data => {
-    console.log(data)
-  })
+async function youtubeAPI(query, num='5') {
+  const vidArr = [];
+  const catArr = defaults.categories;
+  for (let i = 0; i < catArr.length; i++) {
+    let response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${num}&q=${catArr[i]}&key=${keys.apiKey}`)
+    let res = await response.json()
+    vidArr.push(res)
+  }
+  console.log(vidArr)
+  
+  
 }
 
 module.exports = {
