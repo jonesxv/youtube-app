8dcef976327508289103352bd4af721805754cc0
Ryan Jones <ryanjones33@gmail.com>
2019-06-07 09:18:48 -0700

    Handlebars

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+ 
+var app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+ 
+app.listen(3000);
